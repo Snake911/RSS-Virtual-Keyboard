ce898dd07ecbe478892ff1a6bd3ccb3228d81168
Volkov Danil <snake1234567890-1@yandex.ru>
2022-05-04 22:03:56 +0300

    feat: add plugins for Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'public')
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html')
+        }),
+        new CleanWebpackPlugin
+    ]
 }
