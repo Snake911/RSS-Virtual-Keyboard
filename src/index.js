6ffbdb3e649385241a841400c58f443c005d0f7f
Volkov Danil <snake1234567890-1@yandex.ru>
2022-05-08 17:17:45 +0300

    feat: setting eslint, add backspace and delete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './styles/style.scss';
-import keyboardList from './keyboard.json';
+import keysList from './keyboard.json';
+
+const language = localStorage.getItem('lang') || 'en';
+localStorage.setItem('lang', language);
 
 const body = document.querySelector('body');
 const container = document.createElement('div');
@@ -10,58 +13,124 @@ const textarea = document.createElement('textarea');
 container.append(textarea);
 textarea.focus();
 
-const keybordContainer = document.createElement('div');
-keybordContainer.classList.add('keybordContainer');
-container.append(keybordContainer);
+const legend = document.createElement('p');
+legend.innerText = 'Change language: Shift+Alt';
+container.append(legend);
 
-keyboardList.forEach((keyObject) => {
+const keyPressed = [];
+
+let caps = false;
+
+function generationKeyboard(keys, lang, cont) {
+  const oldKeyboard = document.querySelector('.keybordContainer');
+  if (oldKeyboard) {
+    oldKeyboard.remove();
+  }
+
+  const keybordContainer = document.createElement('div');
+  keybordContainer.classList.add('keybordContainer');
+  cont.append(keybordContainer);
+
+  keys.forEach((keyObject) => {
     const key = document.createElement('div');
     key.classList.add('key');
-    key.innerText = keyObject.name || keyObject.en.keyValue;
+    key.innerText = keyObject.name || keyObject[lang].keyValue;
     key.style.gridColumn = `span ${keyObject.length}`;
     key.id = keyObject.code;
+
     key.addEventListener('mousedown', (e) => {
-        e.preventDefault();
-        key.classList.add('press');
-        document.dispatchEvent(new KeyboardEvent('keydown', {code: keyObject.code, key: keyObject.en.keyValue}))
+      e.preventDefault();
+      key.classList.add('press');
+      document.dispatchEvent(new KeyboardEvent('keydown', {
+        code: keyObject.code,
+        key: (keyPressed.includes('ShiftLeft') || keyPressed.includes('ShiftRight') || caps)
+          ? keyObject[lang].keyValueShift
+          : keyObject[lang].keyValue,
+      }));
     });
+
     key.addEventListener('mouseup', (e) => {
-        e.preventDefault();
-        key.dispatchEvent(new KeyboardEvent('keyup'))
-        key.classList.remove('press');
+      e.preventDefault();
+      document.dispatchEvent(new KeyboardEvent('keyup', {
+        code: keyObject.code,
+        key: (keyPressed.includes('ShiftLeft') || keyPressed.includes('ShiftRight') || caps)
+          ? keyObject[lang].keyValueShift
+          : keyObject[lang].keyValue,
+      }));
+      key.classList.remove('press');
     });
     keybordContainer.append(key);
-});
+  });
+}
+
+generationKeyboard(keysList, language, container);
 
 document.addEventListener('keydown', (e) => {
-    if(e.isTrusted) {
-        const pressKey = document.getElementById(e.code);
-        pressKey.classList.add('press');
+  keyPressed.push(e.code);
+  if (e.isTrusted) {
+    const pressKey = document.getElementById(e.code);
+    pressKey.classList.add('press');
+    if (e.code === 'Tab') {
+      e.preventDefault();
+      const value = textarea.value.split('');
+      const position = textarea.selectionStart;
+      value.splice(position, 0, '\t');
+      textarea.value = value.join('');
+      textarea.setSelectionRange(position + 1, position + 1);
+    }
+  } else {
+    const value = textarea.value.split('');
+    let position = textarea.selectionStart;
+
+    if (!e.key) {
+      if (e.code === 'Backspace') {
+        position -= 1;
+        value.splice(position, 1, e.key);
+      } else if (e.code === 'Delete') {
+        value.splice(position, 1, e.key);
+      }
     } else {
-        if(e.key) {
-            const value = textarea.value.split('');
-            const position = textarea.selectionStart;
-            value.splice(position, 0, e.key);
-            textarea.value = value.join('');
-            textarea.setSelectionRange(position + 1, position + 1);
-        }  
+      value.splice(position, 0, e.key);
+      position += 1;
     }
 
-    //arrows
-    if(!e.isTrusted && e.code === 'ArrowLeft' && textarea.selectionStart) {
-        const position = textarea.selectionStart - 1;
-        textarea.setSelectionRange(position, position);
+    textarea.value = value.join('');
+    textarea.setSelectionRange(position, position);
+  }
+
+  // arrows
+  if (!e.isTrusted && e.code === 'ArrowLeft' && textarea.selectionStart) {
+    const position = textarea.selectionStart - 1;
+    textarea.setSelectionRange(position, position);
+  }
+  if (!e.isTrusted && e.code === 'ArrowRight' && textarea.selectionStart) {
+    const position = textarea.selectionStart + 1;
+    textarea.setSelectionRange(position, position);
+  }
+
+  if ((keyPressed.includes('ShiftLeft') || keyPressed.includes('ShiftRight')) && (keyPressed.includes('AltLeft') || keyPressed.includes('AltRight'))) {
+    if (localStorage.getItem('lang') === 'en') {
+      localStorage.setItem('lang', 'ru');
+    } else {
+      localStorage.setItem('lang', 'en');
     }
-    if(!e.isTrusted && e.code === 'ArrowRight' && textarea.selectionStart) {
-        const position = textarea.selectionStart + 1;
-        textarea.setSelectionRange(position, position)
-    }
+
+    generationKeyboard(keysList, localStorage.getItem('lang'), container);
+  }
+
+  if (e.code === 'CapsLock') {
+    caps = !caps;
+  }
 });
 
 document.addEventListener('keyup', (e) => {
-    if(e.isTrusted) {
-        const pressKey = document.getElementById(e.code);
-        pressKey.classList.remove('press');
-    }
+  keyPressed.splice(keyPressed.indexOf(e.code), 1);
+  if (e.isTrusted) {
+    const pressKey = document.getElementById(e.code);
+    pressKey.classList.remove('press');
+  }
 });
 
+textarea.addEventListener('blur', () => {
+  textarea.focus();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,70 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 
 const jsLoaders = () => {
-    const loaders = [{
-        loader: 'babel-loader',
-        options: {
-            presets: [
-              '@babel/preset-env'
-            ],
-            plugins: [
-              '@babel/plugin-proposal-class-properties'
-            ]
-          }
-    }]
+  const loaders = [{
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        '@babel/preset-env',
+      ],
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+      ],
+    },
+  }];
 
-    if(isDev) {
-        loaders.push('eslint-loader')
-    }
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
 
-    return loaders
-}
+  return loaders;
+};
 
 module.exports = {
-    mode: 'development',
-    entry: ['@babel/polyfill',  path.resolve(__dirname, 'src/index.js')],
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'public')
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'src/index.html')
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
-        }),
-        new CopyPlugin({
-            patterns: [
-              {
-                from: path.resolve(__dirname, "src/favicon.ico"),
-                to: path.resolve(__dirname, 'public')
-              },
-            ],
-          }),
+  mode: 'development',
+  entry: [path.resolve(__dirname, 'src/index.js')],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'public'),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use:[MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use:[MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: jsLoaders()
-            }
-        ]
-    }
-}
+  },
+};
